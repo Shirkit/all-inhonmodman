fe6b47800a550aeb01166a34c86341a138476630
pennsu <pennsu@d47efa83-66cc-45f4-b299-ae05fc431935>
2010-06-03 10:19:51 +0000

    I don't think we need business folder anymore so I copy all files and folders into manager
    git-svn-id: https://all-inhonmodman.svn.sourceforge.net/svnroot/all-inhonmodman@21 d47efa83-66cc-45f4-b299-ae05fc431935

---
+++ Manager/manager/ManagerOptions.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package business;
+
+/**
+ *
+ * @author Shirkit
+ */
+public class ManagerOptions {
+
+    private String honFolder;
+
+    public String getHonFolder() {
+        return honFolder;
+    }
+
+    public void setHonFolder(String honFolder) {
+        this.honFolder = honFolder;
+    }
+
+
+
+}
---
+++ Manager/manager/actions/ActionApplyBefore.java
@@ -0,0 +1,55 @@
+package business.actions;
+
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamAsAttribute;
+
+/**
+ * If the specified other mod is enabled, this mod will be applied before it.
+ * @author Shirkit
+ */
+@XStreamAlias("applybefore")
+public class ActionApplyBefore extends Action {
+
+    @XStreamAlias("name")
+    @XStreamAsAttribute
+    private String name; // Other mod
+    @XStreamAlias("version")
+    @XStreamAsAttribute
+    private String version; // Version of the other mod
+
+    public ActionApplyBefore() {
+        setType(APPLY_BEFORE);
+    }
+
+    /**
+     *
+     * @return the name of the Other mod to be applied before this mod.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     *
+     * @return the version of the Other mod to be applied before this mod.
+     */
+    public String getVersion() {
+        return version;
+    }
+
+    /**
+     *
+     * @param name of the Other mod to be applied before this mod.
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     *
+     * @param version of the Other mod to be applied before this mod.
+     */
+    public void setVersion(String version) {
+        this.version = version;
+    }
+}
