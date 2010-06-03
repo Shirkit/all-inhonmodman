ea8f0e525a89e1738197cf2665562e723df61bb5
pennsu <pennsu@d47efa83-66cc-45f4-b299-ae05fc431935>
2010-06-03 13:48:23 +0000

    just for record
    git-svn-id: https://all-inhonmodman.svn.sourceforge.net/svnroot/all-inhonmodman@23 d47efa83-66cc-45f4-b299-ae05fc431935

--- Manager/manager/Mod.java
+++ Manager/manager/Mod.java
@@ -1,7 +1,12 @@
-package manager;
+package com.hon.manager;
+
+import nu.xom.*;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.BufferedInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.zip.*;
 
@@ -32,6 +37,10 @@ public class Mod {
     private String updatecheckurl;
     private String updatedownloadurl;
     private boolean applied;
+
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
 
     /**
      * Mod constructor.
@@ -89,7 +98,7 @@ public class Mod {
         return updatedownloadurl;
     }
 
-    public byte[] getIcon() throws FileNotFoundException {
+    public byte[] getIcon() throws FileNotFoundException, IOException {
         FileInputStream fis = new FileInputStream(modpath+filename);
         ZipInputStream zis = new ZipInputStream(new BufferedInputStream(fis));
         ZipEntry entry;
@@ -98,8 +107,8 @@ public class Mod {
             System.out.println("Extracting: " + entry);
 
             if(entry.getName().equals("icon.png")) {
-                byte data[] = new byte[entry.getSize()];
-                zis.read(data, 0, entry.getSize());
+                byte data[] = new byte[(int)entry.getSize()];
+                zis.read(data, 0, (int)entry.getSize());
 
                 return data;
             }
@@ -107,7 +116,7 @@ public class Mod {
         throw new FileNotFoundException();
     }
 
-    public Document getXML() throws FileNotFoundException {
+    public Document getXML() throws FileNotFoundException, IOException {
         FileInputStream fis = new FileInputStream(modpath+filename);
         ZipInputStream zis = new ZipInputStream(new BufferedInputStream(fis));
         ZipEntry entry;
@@ -116,13 +125,14 @@ public class Mod {
             System.out.println("Extracting: " + entry);
 
             if(entry.getName().equals("mod.xml")) {
-                byte data[] = new byte[entry.getSize()];
-                zis.read(data, 0, entry.getSize());
+                byte data[] = new byte[(int)entry.getSize()];
+                zis.read(data, 0, (int)entry.getSize());
                 System.out.println(data);
-                Builder parser = new BUilder();
-                Document d = parser.build();
+//                Builder parser = new Builder();
+//                Document d = parser.build();
 
-                return data;
+//                return d;
+                return null;
             }
         }
         throw new FileNotFoundException();
