e941ef9ef43ae50dcd322f0e00118927b1ce3dc7
pennsu <pennsu@d47efa83-66cc-45f4-b299-ae05fc431935>
2010-06-03 13:48:59 +0000

    added test.java
    git-svn-id: https://all-inhonmodman.svn.sourceforge.net/svnroot/all-inhonmodman@24 d47efa83-66cc-45f4-b299-ae05fc431935

---
+++ Manager/test/test.java
@@ -0,0 +1,10 @@
+import manager.Mod;
+
+
+public class test {
+
+    public static void main(String[] args) {
+        Mod mod = new Mod("aal.honmod", "/Users/penn/Library/Application Support/Heroes of Newerth/game/mods");
+    }
+
+}
